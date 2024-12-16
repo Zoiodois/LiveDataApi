e5278b23cd7182741732b61a82a0d279e608d69f
Felipe <felipbsr@gmail.com>
2024-12-16 15:47:47 -0300

    livedata succes 1

--- app/Http/Controllers/LivedataController.php
+++ app/Http/Controllers/LivedataController.php
@@ -15,20 +15,27 @@ class LivedataController extends Controller
             'all_data' => $request->all()
         ]);
     
-            // // Validação dos dados recebidos
-            // $validated = $request->validate([
-            //     'UrGHouse'=> ['required', 'integer', 'numeric', 'ascii','digits_between:0,100', 'max:100', 'min:-100' ], 
-            //     'tempGHouse'=> ['required', 'integer', 'numeric', 'ascii','digits_between:0,100', 'max:100', 'min:-100' ], 
-            //     'lum'=> ['required', 'integer', 'numeric', 'ascii','digits_between:0,100', 'max:110', 'min:-100' ], 
-            // ]);
-    
-            // // Armazenamento no banco de dados
-            // Livedata::create($validated);
-    
-            // Retorna uma resposta de sucesso
-            return response()->json([
-                'status' => 'success',
-                'data' => $request->all()
+            // Validação dos dados recebidos
+            $validated = $request->validate([
+                'UrGHouse'=> ['required', 'integer', 'numeric', 'ascii','digits_between:0,100', 'max:100', 'min:-100' ], 
+                'tempGHouse'=> ['required', 'integer', 'numeric', 'ascii','digits_between:0,100', 'max:100', 'min:-100' ], 
+                'lum'=> ['required', 'integer', 'numeric', 'ascii','digits_between:0,100', 'max:110', 'min:-100' ], 
             ]);
+    
+            // Armazenamento no banco de dados
+           if(Livedata::create($validated)){
+               
+               // Retorna uma resposta de sucesso
+               return response()->json([
+                   'status' => 'success',
+                   'data' => $request->all()
+               ]);
+           }
+
+           return respose()->json([
+            'status' => 'fail',
+            
+           ]);
+    
     }
 }
