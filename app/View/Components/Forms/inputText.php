df4c50912652b13543042b2e3171a9b6b5f5a28d
Felipe <felipbsr@gmail.com>
2024-12-23 20:00:26 -0300

    type fix3

--- app/View/Components/Forms/inputText.php
+++ app/View/Components/Forms/inputText.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class inputText extends Component
+class InputText extends Component
 {
     /**
      * Create a new component instance.
--- app/View/Components/productionForm.php
+++ app/View/Components/productionForm.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class productionForm extends Component
+class ProductionForm extends Component
 {
     /**
      * Create a new component instance.
