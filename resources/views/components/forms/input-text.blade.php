d2f86e336b92960877ef9ed7613ae7f78fe44c9d
Felipe <felipbsr@gmail.com>
2024-12-24 10:32:42 -0300

    placeholder bug fix 2

--- resources/views/components/forms/input-text.blade.php
+++ resources/views/components/forms/input-text.blade.php
@@ -1,8 +1,8 @@
 <div>
     <label for="name" class="block text-sm font-medium text-slate-700 mb-1"> {{ $label }} </label>
-    <input  class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+    <input type="{{ $type }}" name="{{$name}}" placeholder="{{$placeholder}}" class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
     focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
     disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
     invalid:border-pink-500 invalid:text-pink-600
-    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 mb-3" type="{{ $type }}" name="{{$name}}" placeholder="{{$placeholder}}">
+    focus:invalid:border-pink-500 focus:invalid:ring-pink-500 mb-3">
 </div>
